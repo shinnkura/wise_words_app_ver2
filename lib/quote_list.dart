8eb97296e1692886e48334a39c19108a795a6feb
shinnkura <n.shinzo1010@icloud.com>
2023-04-24 11:13:12 +0900

    【UI】白を基調したテーマに変更。フォントも日本語をベースとしたものに

--- lib/quote_list.dart
+++ lib/quote_list.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'quote_form.dart';
 
 class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
   @override
   _QuoteListState createState() => _QuoteListState();
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List _quotes = [];
+  final List _quotes = [];
 
-  // 名言を追加する処理を実装する
   void _addQuote(String quote, String author) {
     setState(() {
       _quotes.add({'quote': quote, 'author': author});
@@ -19,35 +20,57 @@ class _QuoteListState extends State<QuoteList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Quote List')),
-      body: ListView.builder(
-        itemCount: _quotes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_quotes[index]['quote']),
-            subtitle: Text(_quotes[index]['author']),
-            onTap: () {
-              // 名言の詳細画面に遷移する処理を実装する
-            },
-          );
-        },
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'WiseWords',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: _quotes.length,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(16),
+                title: Text(
+                  _quotes[index]['quote'],
+                  style: const TextStyle(fontSize: 18),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    _quotes[index]['author'],
+                    style: const TextStyle(
+                        fontSize: 14, fontStyle: FontStyle.italic),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
         onPressed: () async {
-          // 名言フォーム画面に遷移し、結果を待つ
           final result = await Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => QuoteForm(),
             ),
           );
-
-          // 結果がnullでない場合、名言を追加する
           if (result != null) {
             _addQuote(result['quote'], result['author']);
           }
         },
-        child: Icon(Icons.add),
+        child: const Icon(
+          Icons.add,
+          color: Colors.blue,
+        ),
       ),
     );
   }
